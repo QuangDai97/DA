2f5ca16ee6de4961cf68568c652c462dcfcec27b
QuangDai97 <ngoquangdai100397@gmail.com>
2023-02-14 15:10:32 +0700

    layout+SP

---
+++ Controllers/ShopController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectSem3.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Classic()
+        {
+            return View();
+        }
+        public ActionResult OneStar()
+        {
+            return View();
+        }
+        public ActionResult JackPurcell()
+        {
+            return View();
+        }
+        public ActionResult Rubber()
+        {
+            return View();
+        }
+        public ActionResult Chuck1970()
+        {
+            return View();
+        }
+    }
+}
